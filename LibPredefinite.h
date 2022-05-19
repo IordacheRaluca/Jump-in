ba91d8759df0d5d9afb2f4140922d24304a3f58b
IordacheRaluca <iordache.raluca03@gmail.com>
2022-05-19 11:27:40 +0300

    initial commit

---
+++ LibPredefinite.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <graphics.h>
+#include <winbgim.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "graphics.h"
+#include <dos.h>
+#include <windows.h>
+#include <mmsystem.h>
+
+
